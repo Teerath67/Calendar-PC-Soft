c73817b2f220965f6cc4ecded0aa145adb51e4ec
Teerath67 <teerath671@gmail.com>
2023-01-09 11:36:42 +0100

    Setup the Calendar with new Color Scheme, Dark theme, and switch

--- app/src/main/java/com/example/calendarpcsoft/MainActivity.java
+++ app/src/main/java/com/example/calendarpcsoft/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.calendarpcsoft;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize the Switch
+        SwitchMaterial themeSwitch = findViewById(R.id.themeSwitch);
+
+        //switch theme when user wishes
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    compoundButton.setText(getResources().getString(R.string.dark_theme));
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    compoundButton.setText(getResources().getString(R.string.light_theme));
+                }
+            }
+        });
+
+
+        //is night mode is on by Default
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        boolean nightMode = currentNightMode == Configuration.UI_MODE_NIGHT_YES;
+        themeSwitch.setChecked(nightMode);
+
+        if (nightMode) {
+            themeSwitch.setText(getResources().getString(R.string.dark_theme));
+        }
     }
 }
